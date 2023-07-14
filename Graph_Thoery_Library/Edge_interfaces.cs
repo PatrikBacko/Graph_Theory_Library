aed2ab2a3497488ffb9bc2db980f855955767a58
Buciak <patrik.backo1@gmail.com>
2023-07-14 13:15:28 +0200

    changes

--- Graph_Thoery_Library/Edge_interfaces.cs
+++ Graph_Thoery_Library/Edge_interfaces.cs
@@ -8,10 +8,29 @@ using System.Threading.Tasks;
 namespace Graph_Thoery_Library
 {
 	public interface IEdge {
-
+	IVertex Vertex1 { get; }
+	IVertex Vertex2 { get; }
 	}
 
-	public interface IWeightedEdge<TWeightType> where TWeightType : INumber<TWeightType> {
-
+	public interface IOrientedEdge{
+	IVertex InVertex { get; }
+	IVertex OutVertex { get; }
 	}
+
+	public class Edge : IEdge
+	{
+		public IVertex Vertex1 { get; set; }
+		public IVertex Vertex2 { get; set; }
+	}
+
+	public class OrientedEdge : IOrientedEdge 
+	{
+		public IVertex InVertex { get; private set; }
+		public IVertex OutVertex { get; private set; }
+	}
+
+
+	//public interface IWeightedEdge<TWeightType> where TWeightType : INumber<TWeightType> {
+
+	//}
 }
--- Graph_Thoery_Library/Vertex_interfaces.cs
+++ Graph_Thoery_Library/Vertex_interfaces.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
@@ -7,12 +8,41 @@ using System.Threading.Tasks;
 
 namespace Graph_Thoery_Library
 {
-	public interface IVertex{
 
-	}
-
-	public interface IWeightedVertex<TWeightType> where TWeightType : INumber<TWeightType>
+	public interface IVertex
 	{
-
+		string Name { get; }
 	}
+
+	public class Vertex : IVertex{
+		public string Name { get; init; }
+
+		protected int Index { get; set; }
+
+		public Vertex(string Name) {
+		this.Name = Name;
+		Index = -1;
+		}
+	}
+
+
+	//public interface IVertex<TName>
+	//{
+	//	TName Name { get; set; }
+	//}
+
+	//public interface IWeightedVertex<TWeightType> where TWeightType : INumber<TWeightType>
+	//{
+
+	//}
+
+	//public class Vertex<TName> : IVertex<TName>
+	//{
+	//	public TName Name { get; set; }
+	//}
+
+	//public class WeightedVertex<TWeightType> : IWeightedVertex<TWeightType> where TWeightType : INumber<TWeightType> 
+	//{
+		
+	//}
 }
